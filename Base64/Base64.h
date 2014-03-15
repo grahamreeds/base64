2606ab6b9243bfbeda764c0a49105e5701d60c04
Graham Reeds <graham.reeds@gmail.com>
2014-03-15 18:37:34 +0000

    Base64 correctly encodes multibyte strings

--- Base64/Base64.h
+++ Base64/Base64.h
@@ -1,21 +1,64 @@
 #pragma once
+#include <string>
 
 namespace
 {
-	template< class Container >
-	class encoder_
+	template< class Container = std::string>
+	class encoder
 	{
 	public:
-		encoder_(Container c) : ref_(c) { }
-		~encoder_() { }
-		void operator()(unsigned char byte)	{ }
+		encoder(Container& c) 
+			: ref_(c), bits_(0), bits_remaining_(0) { }
+		~encoder() 
+		{
+			if (bits_remaining_ > 0)
+			{
+				bits_ = (bits_ << 8); // | 0x00;
+				bits_remaining_ += 8;
+
+				const int shift = bits_remaining_ - 6;
+				const int mask = 0x3f << shift;
+				const int index = (bits_ & mask) >> shift;
+
+				const char out_byte = alphabet_[index];
+				ref_.push_back(out_byte);
+			}
+
+			while (ref_.size() % 4)
+			{
+				ref_.push_back('=');
+			}
+
+		}
+		void operator()(unsigned char byte)	
+		{
+			bits_ = (bits_ << 8) | byte;
+			bits_remaining_ += 8;
+			while (bits_remaining_ > 4)
+			{
+				const int shift = bits_remaining_ - 6;
+				const int mask = 0x3f << shift;
+				const int index = (bits_ & mask) >> shift;
+
+				bits_ = bits_ & ~mask;
+				bits_remaining_ -= 6;
+
+				const char out_byte = alphabet_[index];
+				ref_.push_back(out_byte);
+			}
+		}
+		encoder& operator=(const encoder&) = delete;
 	private:
-		Container& ref_;
+		Container&	ref_;
+		int			bits_;
+		int			bits_remaining_;
+		const std::string alphabet_ = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 	};
 }
-
+/*
 template< class Container >
 encoder_<Container> encoder(Container& c)
 {
 	return encoder_<Container>(c);
 }
+*/
