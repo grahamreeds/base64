eb3881c2b5d5e7130834629ef809e9258f7e178c
Graham Reeds <graham.reeds@gmail.com>
2014-03-15 14:28:57 +0000

    encoder handles empty strings correctly

--- Base64/Base64.h
+++ Base64/Base64.h
@@ -1,6 +1,21 @@
 #pragma once
 
-int TempFunc(void)
+namespace
 {
-	return 1;
+	template< class Container >
+	class encoder_
+	{
+	public:
+		encoder_(Container c) : ref_(c) { }
+		~encoder_() { }
+		void operator()(unsigned char byte)	{ }
+	private:
+		Container& ref_;
+	};
+}
+
+template< class Container >
+encoder_<Container> encoder(Container& c)
+{
+	return encoder_<Container>(c);
 }
--- TestBase64/TestBase64.cpp
+++ TestBase64/TestBase64.cpp
@@ -1,7 +1,12 @@
 #include "UnitTest++.h"
 #include "Base64.h"
+#include <algorithm>
 
-TEST(Test)
+TEST(CheckEncoderHandlesZeroByteString)
 {
-	CHECK(false);
+	const std::string unencoded_string("");
+	std::string encoded_string;
+	std::for_each(unencoded_string.cbegin(), unencoded_string.cend(), encoder(encoded_string));
+	CHECK_EQUAL(0U, encoded_string.size());
+	CHECK_EQUAL("", encoded_string);
 }
